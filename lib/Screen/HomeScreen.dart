47c493bf9fedac2df972efd685252f8f0f733b73
SAURABH0504 <saurabhpatidar69@gmail.com>
2021-03-12 18:00:28 +0530

    Home Screen Updated

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freelancekisan_user/Service/AuthService.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -22,16 +21,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(22.544,88.340),
-        zoom: 15,
+      body: Stack(
+        children: [
+          GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(22.544,88.340),
+            zoom: 15,
+          ),
+            onMapCreated: _onMapCreated,
+            markers: _markers,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Your Address',
+                    focusedBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    focusColor: Colors.white,
+                    disabledBorder: InputBorder.none,
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                width: 300,
+                child: TextButton(
+                  child: Text('Select Category',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-        onMapCreated: _onMapCreated,
-        markers: _markers,
-      ),
 
 
 
-      appBar: AppBar(title: Text('Search')),
+      appBar: AppBar(title: Text('Search'),backgroundColor: Colors.green,),
 
       drawer: Drawer(
         child: ListView(
@@ -55,10 +97,9 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
             ListTile(
-              title:Text('Logout'),
-              onTap: (){
+              title:TextButton(child: Text('Logout',style: TextStyle(color: Colors.red),),onPressed: (){
                 AuthService().signOut();
-              },
+              },),
             ),
 
           ],
