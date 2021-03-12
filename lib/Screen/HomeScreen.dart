af0bb855232963f0454f9bede1d137c708b5330d
RitwikMehta <ritwikmehta28@yahoo.in>
2021-03-12 12:17:33 +0530

    Map added.

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:freelancekisan_user/Service/AuthService.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  Set<Marker> _markers ={};
+
+  void _onMapCreated(GoogleMapController controller){
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId('id-1'),position: LatLng(22.544,88.340)
+      ),);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(22.544,88.340),
+        zoom: 15,
+      ),
+        onMapCreated: _onMapCreated,
+        markers: _markers,
+      ),
+
+
+
       appBar: AppBar(title: Text('Search')),
-      body: Center(child: Text('My Page!')),
+
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
